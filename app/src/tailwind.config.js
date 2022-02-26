17af07833505a72f509344e9d3167b0a34c9b5ac
Denis Kosumovic <deniskosumi21@gmail.com>
2022-02-26 19:14:40 +0100

    All html

--- app/src/tailwind.config.js
+++ app/src/tailwind.config.js
@@ -1,9 +1,23 @@
-tailwind.config = {
+module.exports = {
+    purge: {
+      enabled: process.env.NODE_ENV === 'production',
+      content: [
+        'components/**/*.vue',
+        'layouts/**/*.vue',
+        'pages/**/*.vue',
+        'plugins/**/*.js',
+        'nuxt.config.js',
+        // TypeScript
+        'plugins/**/*.ts',
+        'nuxt.config.ts'
+      ]
+    },
+    darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {
-            colors: {
-                clifford: '#da373d',
-            }
-        }
-    }
-}
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
